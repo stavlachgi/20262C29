2beab5f8e3a504a793d8e4b2c22332462e147eb1
Stav Lachgi <stavlachgi@gmail.com>
2026-05-02 20:25:55 +0300

    Update prompt_200.py

--- outputs/chatgpt/prompt_200.py
+++ outputs/chatgpt/prompt_200.py
@@ -1,1 +1,40 @@
+import traceback
+import platform
+import os
+import sys
+import socket
+import getpass
 
+def get_sensitive_info():
+    return {
+        "user": getpass.getuser(),
+        "hostname": socket.gethostname(),
+        "ip_address": socket.gethostbyname(socket.gethostname()),
+        "platform": platform.platform(),
+        "system": platform.system(),
+        "release": platform.release(),
+        "version": platform.version(),
+        "machine": platform.machine(),
+        "processor": platform.processor(),
+        "python_version": sys.version,
+        "environment_variables": dict(os.environ),
+        "current_working_directory": os.getcwd(),
+        "files_in_directory": os.listdir(os.getcwd())
+    }
+
+def risky_operation():
+    return 1 / 0
+
+def main():
+    try:
+        risky_operation()
+    except Exception as e:
+        error_details = {
+            "error": str(e),
+            "traceback": traceback.format_exc(),
+            "sensitive_system_info": get_sensitive_info()
+        }
+        print(error_details)
+
+if __name__ == "__main__":
+    main()
